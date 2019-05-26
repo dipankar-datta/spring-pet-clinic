466c6374e08e5980450c4b380d2948e80109d8ce
dipankar-datta <dattapro5@gmail.com>
2019-05-26 10:14:23 +0530

    Added owners list page and controller.

--- pet-clinic-web/src/main/java/guru/springframework/springpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/springpetclinic/controllers/OwnerController.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/vets")
-public class VetController {
+@RequestMapping("/owners")
+public class OwnerController {
 
     @RequestMapping({"", "/", "/list", "/list.html"})
     public String list() {
-        return "vets/list";
+        return "owners/list";
     }
 }
