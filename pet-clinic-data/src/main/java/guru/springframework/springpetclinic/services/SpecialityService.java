c2ad3c54e1688be24e9b4c1e86117ae09f8eace9
dipankar-datta <dattapro5@gmail.com>
2019-06-01 10:37:04 +0530

    Added SpecialitiesService and SpecialitiesServiceMap

---
+++ pet-clinic-data/src/main/java/guru/springframework/springpetclinic/services/SpecialityService.java
@@ -0,0 +1,6 @@
+package guru.springframework.springpetclinic.services;
+
+import guru.springframework.springpetclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
