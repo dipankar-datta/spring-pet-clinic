ba382e8adea519b392df353a27924e426f957728
dipankar-datta <dattapro5@gmail.com>
2019-06-10 19:36:11 +0530

    Implemented repositories.

---
+++ pet-clinic-data/src/main/java/guru/springframework/springpetclinic/repositories/SpecialityRepository.java
@@ -0,0 +1,7 @@
+package guru.springframework.springpetclinic.repositories;
+
+import guru.springframework.springpetclinic.model.Speciality;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SpecialityRepository extends CrudRepository<Speciality, Long> {
+}
